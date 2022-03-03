c36eb4100e94aab689aeec1eaf4bcc72842992ee
jbalmonte <jbalmonte45@gmail.com>
2022-03-03 16:23:28 +0800

    Added reflection list

---
+++ js/reflections.js
@@ -0,0 +1,78 @@
+
+
+
+
+export default [
+    {
+        name: 'Faith Angela Marco',
+        img: 'balmonte.png',
+        role: 'General Manager',
+        a: "Throughout the activity, I have learned that Adobe Photoshop is very powerful software especially when you know how to use it. The skills you acquire while using the software is very helpful in some cases and it is very rewarding as well. It is very pleasing when you finish a project or task in Adobe Photoshop especially if you are just a beginner and the project or outcome looks very nice.",
+        b: "If you enjoy and are passionate about Adobe Photoshop, it may be a really interesting and enjoyable way of life. However, if you force yourself to like it, it may be alright at first, but you will regret it in the long run, and you will not have time to pursue the route you truly desire. That is why it is critical to have a clear idea of what you want to do with your life so that you do not wind up on a detour. Whatever happens, the most important thing is that you get to learn a lot of new things along the route and still get to arrive at your destination. That is why it is critical to have a clear idea of what you want to do with your life so that you do not wind up on a detour. Whatever happens, the most important thing is that you learn a lot of new things along the way while still arriving at your goal. Even if it takes a long time, it's better to be late than never.",
+        c: "One situation in which I can apply what I've learnt from this assignment is when I apply for a job as a graphic designer in a business. Now that I'm equipped with the skills and information from this course, I'll be able to utilize what I've learned when they want me to create logos, PR packages, and other materials. That way, I'll be able to do something I'm passionate about while still earning money, which is unique."
+    },
+    {
+        name: 'Joshua Balmonte',
+        img: 'balmonte.png',
+        role: 'Developer',
+        a: "I learned a lot after this activity in Photoshop. We used various tools to achieve the desired output, and I was assigned to design the user interface. First, I applied the text tool to the menu section and added different styles and sizes to the landing page. Second, I used different color styles, such as brush tools, to make the image lively. Third, I added a masking tool to design the laptop image to achieve a rounded border radius. Lastly, I undo and redo things that are very important when editing to go back to their previous or next state.",
+        b: "Adobe Photoshop is a way of life because it promotes three essential values: creativity, persistence, and cooperation. First is creativity; using your visual and imagination skills, you can think of the most fantastic idea appropriate to the task. The second is persistence; by using various tools and adjustments, using Photoshop is not an easy task because it requires every bit of patience to achieve the most detailed and quality work. Lastly, cooperation is an essential value since the activity is a group task, communication and trust with each other's work bring out the best of everyone.",
+        c: "Adobe Photoshop is not just for schoolwork; it's also necessary for everyday jobs involving picture manipulation and creative designs, especially in the IT industry. For instance, if my boss asked me to design a logo for them, I would gladly use picture editing tools such as Photoshop to create an icon and apply the typeface tool. The logo could combine images, colors, shapes, and other elements. Furthermore, I would use the software's excellent features when they assigned me packaging design.Finally, knowing Photoshop is always beneficial because it requires almost any editing task."
+    },
+
+    {
+        name: 'Jeno Kyle Recio',
+        img: 'balmonte.png',
+        role: 'Developer',
+        a: "Since I am assigned in IT shop task, there are a lot of things that I need to edit, from scratch to our mockup store (output). I've learned to utilize the Rectangular Marque Tool. For me, this tool is the most used tool that I used in editing our mockup store. I also learned to Inverse the selection to reverse the selected area and rasterize the layer to be able to edit it. Furthermore, I learned how to utilize the Color Balance and Hue and Saturation settings. It is fun playing with its colors and satisfying myself with it. In Addition, I learned to utilize the Gradient Tool for the shadows in our mockup store. In every assessment that the professor wants us to perform, there are a lot of things that I've learned. It also enhances and utilizes the knowledge that I gained. But I know it is still not enough, I still consider myself as a beginner in Adobe Photoshop.",
+        b: "It is indeed that Adobe Photoshop is very useful nowadays. It has a lot of functions that can be used in different ways, from personal use to business industry use. The professor gave us four different tasks in a project. It consists of beautification, business logo, IT shop design, and system design. As an IT student, I may encounter these tasks in the future. I may be the one who will edit the picture of my customer; I may be the one who designs and creates the business logo for a company; I may be the one who designs a shop for my client; and/or I may be the one who designs a system. This subject made me realize the versatility of Adobe Photoshop and I'm happy that I once experienced these kinds of tasks.",
+        c: "I've learned many things in Adobe Photoshop, from simple to intermediate level of editing of images. As an IT student, I think this knowledge that I gained can help me when I'm applying for a job. Some companies require basic knowledge in Photoshop to be able to accept their applicant. If I apply for a system designer position, I can use the knowledge that I gained from this subject. Furthermore, Adobe Photoshop is not only for complex editing. I can also use Photoshop when I want to remove a pimple from my face, remove a background of an image, add a simple filter to my photo, etc. I admit that Adobe Photoshop is not easy to learn, but it is fun to have knowledge and experience about it."
+    },
+
+    {
+        name: 'Ezekiel Patulot',
+        img: 'balmonte.png',
+        role: 'Editor',
+        a: "Just like in the last activity, I created logo for our group. I learned that there are many things you can do in Photoshop that can help you in real life. It is important because you can earn a lot of money if you got a commission from a big company or any other brand. And you can also use it for your own business, which will save you a lot of money because you don't need to pay other people to make logo for your business. After the activity I have learned how to make a logo and also collaborate with your team to make it better.",
+        b: "As I already stated above, having skills in photoshop can be very useful. You can become a freelance designer for logo, signs, etc. and earn a lot of money. Maybe you can make it a fulltime job, because it really pays a good amount of money. It will be helpful to you and especially to your family, and you can also help businesses as you create their main face.",
+        c: "We can apply photoshop in real life, especially in jobs. For example, graphic designers and artists. They use mainly photoshop where they can create artworks, they earn a lot because of that and also of their skills. So for me this activity is very helpful because it opens our mind to become creative so we can use it someday."
+    },
+
+    {
+        name: 'Archie Dela Paz',
+        img: 'balmonte.png',
+        role: 'Editor',
+        a: "By using Adobe Photoshop, I've learned various of tools needed in order to make the image/photo better as I was assigned to do the beautification in my group. The first thing I did after putting an image, I zoomed in the photo to see the parts that I need to fix by using a healing brush tool I traced the parts of marks like pimples and freckles and after that to make the photo presentable I changed its skin tone to make it look better. First is by using the Quick Selection Tool you can easily trace the parts you want to change. It is not that easy though because I find it hard since I'm new on doing things like this but I made it.",
+        b: "As we work together as a group that's the first thing that pops up in my mind, our cooperation, the way we share our ideas, the way we make it fair for everyone by means of voting for tasks, because as we get older some of these things will matter and will make sense like when we're already working in a company, we can easily feel at ease working with someone and share ideas with them. Also, this project with my groupmates made me feel more creative as I worked on my part in beautification team.",
+        c: "Adobe Photoshop can be used for some other things too, not just by this project. Also, I can also apply the knowledge I had here for example I'm already working in a company, It would be easier for me to use Adobe Photoshop than before because I already made a project with the help of some people and so much more of tools that we can use, we can also design a company logo, UI, and lastly I can easily edit photos of faces that I want to change for a better quality and smoother result."
+    },
+
+    {
+        name: 'Marc Abi Aquino',
+        img: 'balmonte.png',
+        role: 'Editor',
+        a: "I learned how to use the various tools in Photoshop. Fill, reduce opacity, modify color, and delete an object from a prior image were all possible. I learned how to apply distort, perspective, and warp as well. By completing this exercise, I was able to have a better understanding of how layers work. It was challenging at first since I only knew the fundamentals, but it became easier as my skills increased. We were able to complete the project as a group due to everyone's excellent communication and coordination.",
+        b: "Photoshop is fascinating to use since it essentially includes all of the tools you'll ever need, and you don't have to be a pro to use it. Adobe Photoshop is used to design and edit the majority of our deliverables. Because of the software's adaptability, we were able to add several editing tools. You may use it to make digital 'art' from scratch, retouch images, and construct architectural blueprints.",
+        c: "Photoshop may be used for a variety of tasks, including editing scanned or digital images. To give the photographs a fresh look, we can modify the color tone and brightness. With Photoshop, we may remove or modify the backgrounds of our photographs. Additionally, Photoshop allows us to professionally fix, sharpen, and improve images."
+    },
+
+
+    {
+        name: 'Lemnuel Lumaban',
+        img: 'balmonte.png',
+        role: 'Editor',
+        a: "In this activity I experienced using it into a real-life application which focuses on the business, marketing side specifically. In this activity, I was assigned to design the user interface of our website which requires front-end developing skills. Using photoshop made it easier and efficient for it got all I need like texts, objects, etc. With that I learned how important photoshop in advertising is, making it more eye-catching and appealing in order to attract more customers. Being said photoshop has much to offer and as not an artistic person experience is needed in order to do more.",
+        b: "Adobe Photoshop is an essential tool for customizing and editing photos to satisfy one's need. Access to this software are most beneficial to designers, web developers, graphic artists, photographers, and creative professionals. Photoshopping skills are very handy that it opens a lot of opportunity which I envy. Despite of not being artistic, I really want to learn it somehow because I know it would be useful in my career.",
+        c: "I might say I can apply this for old photos which have been damaged or deteriorated as time passes by, I can use photoshop to somehow make it new and pleasing through enhancing some part of it. This is so helpful especially if that photo is so memorable, and you can't afford to lose it. Another scenario would be for advertisement for the coming election. Most of the candidates would rather use posters and banners to advertise their advocates, photoshopping is a timely thing for this kind of event."
+    },
+
+    {
+        name: 'Rissy Mae Alcuizar',
+        img: 'alcuizar.png',
+        role: 'Editor',
+        a: "In general, I learned so many things on using the application that is very useful in enhancing and magically editing images using different tools. I learned how to use the Spot Healing Brush Tool in removing unpleasant stuff on the face like pimples. I also learned the use of layer mask, content-aware fill type, perspective tool, warping tool and above all else using the move tool to select objects or parts in the image that you can transfer to another canvas window. I also had fun using different blending effects. I especially use this when I want my object to have shadow, gradient fill, and metal-like effect. Overall, learning how to use the tools in the application is a skill I can use in a different daily situation.",
+        b: "In the modern world that we are living in today, editing an image is not a surprise anymore. This has become a part of life for everyone, especially those that are in the marketing and advertising industry. I can’t imagine how their products are to be published publicly with only having the raw images or videos they took originally without any alterations made. These will surely look unappealing and unattractive to people and they might not make sales anymore.<br>We can say that we almost rely our life in making our images look more aesthetic, which in this case the application has become our way of life. We can use this tool in different ways like applying filters in the image, making a product from scratch, and making clips. Without the application, you can still do these things using other applications, but Adobe Photoshop is the best application we can use which has all the tools we need to alter our images like professionals.",
+        c: "In real life situations, I use Adobe Photoshop in, for example from our activity, removing pimples and scars. As a person who is living her life with pimples on her face, I find this application very useful in helping me remove my pimples and clear my face to look more neat and appealing. I was also among those people who get shy about showing their face and posting it to social media platforms with acnes visible on the face. So, I think that I will always use this tool in altering my face until my face is free from these annoying bumps and spots in reality."
+    }
+
+]
--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,4 @@
-/*!
-* Start Bootstrap - Agency v7.0.10 (https://startbootstrap.com/theme/agency)
-* Copyright 2013-2021 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-agency/blob/master/LICENSE)
-*/
-//
-// Scripts
-// 
+import reflections from './reflections.js'
 
 window.addEventListener('DOMContentLoaded', event => {
 
@@ -51,4 +44,53 @@ window.addEventListener('DOMContentLoaded', event => {
         });
     });
 
+
+    function reflection({ img, name, role, a, b, c }) {
+        return `
+        <hr>
+        <div class="row my-5">
+            <div class="col-lg-4">
+                <div class="team-member">
+                    <img class="mx-auto rounded-circle" src="assets/img/profile/${img}" alt="${name}" />
+                    <h4>${name}</h4>
+                    <p class="text-muted">${role}</p>
+                    <a class="btn btn-dark btn-social mx-2" href="#!"><i class="fab fa-twitter"></i></a>
+                    <a class="btn btn-dark btn-social mx-2" href="#!"><i class="fab fa-facebook-f"></i></a>
+                    <a class="btn btn-dark btn-social mx-2" href="#!"><i class="fab fa-linkedin-in"></i></a>
+                </div>
+            </div>
+            <div class="col-lg-8">
+                <ol type="1" class="small">
+                    <li>
+                        <strong>
+                            What are the things you've learn in Adobe Photoshop?
+                        </strong>
+                        <p>
+                           ${a}
+                        </p>
+                    </li>
+                    <li>
+                        <strong>
+                            Adobe Photoshop as a way of life?
+                        </strong>
+                        <p>
+                          ${b}
+                        </p>
+                    </li>
+                    <li>
+                        <strong>
+                            Cite a scenario on how you will apply this in real life situation?
+                        </strong>
+                        <p>
+                            ${c}
+                        </p>
+                    </li>
+                </ol>
+            </div>
+        </div>
+    `
+    }
+
+    document.querySelector('#reflection-list').innerHTML = reflections.map(reflection).join``
 });
+
